a4a8772c6e3078606b1a611cb083a2b1b2ab242c
VAIBHAVJOE101 <vaibhav251200@gmail.com>
2022-04-09 15:13:42 +0530

    mapbox

--- client/src/Pages/Ambulance.js
+++ client/src/Pages/Ambulance.js
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import Map from '../components/Map'
 import { Box, Typography, Container, Button, Fab } from '@mui/material';
-import {NavigationIcon} from '@mui/icons-material';
-
 export default function Ambulance() {
    const [selected, setSelected] = React.useState(true);
    return (
@@ -12,7 +10,6 @@ export default function Ambulance() {
          </Box>
          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
             <Fab variant="extended" color="error" aria-label="add" sx={{width:[200,300,400,500],fontSize:24,height:60}}>
-            <NavigationIcon sx={{ mr: 1 }} />   
             Start Critical Ride
             </Fab>
          </Box>
