0d1903de93d9436f0fd73b79136ffc03ae870e26
deveshteotia12 <devesh.teotia12@gmail.com>
2022-04-10 05:50:56 +0530

    final

--- client/src/Pages/User.js
+++ client/src/Pages/User.js
@@ -1,13 +1,41 @@
 import * as React from 'react';
-import Map from '../components/Map'
+import Map from '../components/Map';
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { Box } from '@mui/material';
+var socket;
 
 function User() {
-   return (
-      <Box sx={{ height: 1100, mb: 5 }}>
-            <Map user="user"/>
-         </Box>
-   )
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [ambulanceData, setAmbulance] = useState([]);
+  React.useEffect(() => {
+    socket = io('http://localhost:5000/');
+    // socket.emit('joinAmbulance', { id: rID });
+  }, []);
+
+  React.useEffect(() => {
+    navigator.geolocation.getCurrentPosition(pos => {
+      setLatitude(pos.coords.latitude);
+      setLongitude(pos.coords.longitude);
+    });
+    console.log(latitude);
+    if (latitude !== null && longitude !== null) socket.emit('carData', { coordinates: [latitude, longitude] });
+  }, [latitude, longitude]);
+
+  React.useEffect(() => {
+    socket.on('ambData', data => {
+      console.log('I am listening');
+      // console.log(data);
+      setAmbulance([...ambulanceData, data]);
+    });
+  });
+
+  return (
+    <Box sx={{ height: 1100, mb: 5 }}>
+      <Map user="user" ambulanceData={ambulanceData} longitude={longitude} latitude={latitude} />
+    </Box>
+  );
 }
 
-export default User
+export default User;
